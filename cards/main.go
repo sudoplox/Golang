bdbe058e9e30d35a0529ffdfecd2c8b5162ce295
Sudhanshu Mohan <sudhanshu17110@iiitd.ac.in>
2021-06-17 22:41:12 +0530

    variable declaration

---
+++ cards/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var card string = "Ace of Spades"
+	
+	/*
+	var-> variable
+	card-> name
+	string-> only string type will be assigned ever
+	
+	go is statically typed language like c++, java
+
+	available types -> bool, string, int, float64
+	
+	*/
+	
+	card := "Ace of Spades"
+	// := for NEW VARIABLE, initialisation
+
+	card = "Five of Diamonds"
+	// = for REASSGINMENT
+
+	fmt.Println(card)
+
+}
