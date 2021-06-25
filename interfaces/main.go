b7f73285cecc2da9f6f01efca653c9e20f03133c
Sudhanshu <sudhanshu.mohan@go-mmt.com>
2021-06-25 12:31:26 +0530

    rules of interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,6 +2,19 @@ package main
 
 import (	"fmt")
 
+// bot is interface type, we cant create variable of it
+type bot interface{
+	//fun_name(arg_type) return_type
+	getGreeting() string 
+	
+
+	// if you are a type and you have a function called
+	// getGreeting and u return a string, 
+	// then you can be called a bot
+	// thus you can use the function printGreeting too
+}
+
+// _bots are concrete type, we can create variable of it
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -10,25 +23,19 @@ func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 
+}
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 // we can omit the variable name since we arent using
 func (englishBot) getGreeting() string{
-	// veru custom logic
+	// very custom logic
 	return "Hi there!"
 }
 
 func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
-
-// no function overloading?
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-// func printGreeting(sb spanishBot){
-// 	fmt.Println(sb.getGreeting())
-// }
-
