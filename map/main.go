e27055910bf363ee743b2fcaf415818c3cbc79f4
Sudhanshu <sudhanshu.mohan@go-mmt.com>
2021-06-25 10:08:59 +0530

    manipulate map

--- map/main.go
+++ map/main.go
@@ -6,9 +6,18 @@ import (
 
 
 func main() {
-	colors := map[string]string{
-		"red": "#ff0000",
-		"green": "#fbf723",
-	}
-	fmt.Println(colors)
+
+	//map[key type]value_type
+	var colors1 map[string]string
+	//or
+	colors2 := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red": "#ff0000",
+	// 	"green": "#fbf723",
+	// }
+	colors2["white"] = "#ff0000"
+
+	delete(colors2,"white") //to delete a pair in map
+	fmt.Println(colors1,colors2)
 }
