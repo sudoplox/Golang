2cecbac7c64cdcb829dfe88d41f89d7126eda682
Sudhanshu <sudhanshu.mohan@go-mmt.com>
2021-06-28 10:55:13 +0530

    channel implementation

--- channels/main.go
+++ channels/main.go
@@ -13,16 +13,24 @@ func main() {
 		"http://facebook.com",
 		"http://stackoverflow.com",
 	}
+	c := make(chan string) //create a channel
+
 	for _,link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+		// concurrency is not parallelism
+		// put go keyword infront of function calls
+
 	}
+	fmt.Println( <- c ) // wait for value to be sent into the channel, when we get then log it immediately
 }
 
-func checkLink(link string){
+func checkLink(link string, c chan string){
 	_,err := http.Get(link)
 	if err != nil {
 		fmt.Println(link,"might be down!")
+		c <- "might be down!" // send string to the channel c
 		return
 	}
 	fmt.Println(link,"is up!")
+	c <- "Yup it's up!"	// send string to the channel c
 }
