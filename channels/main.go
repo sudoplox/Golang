5996b167bd8798ff2b81b03bf2eadd203931c558
Sudhanshu <sudhanshu.mohan@go-mmt.com>
2021-06-28 11:39:36 +0530

    literals and channels gotcha

--- channels/main.go
+++ channels/main.go
@@ -24,22 +24,22 @@ func main() {
 	// fmt.Println( <- c ) // wait for value to be sent into the channel, when we get then log it immediately
 	
 	for l := range c {
-		go checkLink( l,c)
-		
+		go func (link string)  { // to fix the prob
+			time.Sleep(time.Second)
+			checkLink(link,c)  // prob: l variable from main routine but used in child routine. pass l value instead
+		}(l) // to fix the prob
 	}
 }
 
 func checkLink(link string, c chan string){
 	_,err := http.Get(link)
-	
-	time.Sleep(time.Second)
 
 	if err != nil {
 		fmt.Println(link,"might be down!")
 		c <- link // send string to the channel c
 		return
 	}
-	
+
 	fmt.Println(link,"is up!")
 	c <- link	// send string to the channel c
 }
